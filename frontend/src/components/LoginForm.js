50ae9374a2341232b3e6674d8033d3cf8ef119b5
mleger45 <mleger45@gmail.com>
2021-01-03 03:01:26 -0300

    fixed loginform; redirection to dashboard

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,17 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import clsx from "clsx";
-import IconButton from "@material-ui/core/IconButton";
-import Input from "@material-ui/core/Input";
-import FilledInput from "@material-ui/core/FilledInput";
-import OutlinedInput from "@material-ui/core/OutlinedInput";
-import InputLabel from "@material-ui/core/InputLabel";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import FacebookLogin from 'react-facebook-login';
 import GoogleLogin from 'react-google-login';
 import FacebookIcon from '@material-ui/icons/Facebook';
@@ -29,13 +20,18 @@ const useStyles = makeStyles((theme) => ({
 
 function LoginForm() {
   const classes = useStyles();
+  const history = useHistory();
 
   const responseFacebook = (response) => {
+    //post to some database, redirect to dashboard
     console.log(response);
+    history.push('/dashboard');
+
   }
 
   const responseGoogle = (response) => {
     console.log("received", response);
+    history.push('/dashboard');
   }
 
   
